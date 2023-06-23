bf730ce54d5c6b282a5950d2b926fb1a2653c6a4
DimonAgon <dimon2003agon@gmail.com>
2023-06-23 22:39:21 +0300

    remove extra imports

--- src/app/password-form/password-form.component.ts
+++ src/app/password-form/password-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, Query, QueryList, ViewChildren} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 
 import {FormControl} from "@angular/forms";
 
